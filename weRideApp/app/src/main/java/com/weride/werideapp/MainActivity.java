a0f803331e49cff21656c1f0fe55f3921541c78a
prajwal shrestha <prajwalshrestha@prajwals-MBP.eduroam.student>
2017-02-09 10:49:01 +0000

    maps added to start race page

--- weRideApp/app/src/main/java/com/weride/werideapp/MainActivity.java
+++ weRideApp/app/src/main/java/com/weride/werideapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.weride.werideapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gotomap(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
+
+
