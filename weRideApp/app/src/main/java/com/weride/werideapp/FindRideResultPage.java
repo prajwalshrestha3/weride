5b8c84f69aea558172afacc972e1ef380368d497
Joseph Cooper <josephcooper@Josephs-MBP.lan>
2017-04-23 18:38:51 +0100

    Added a list view for results of search to find ride page

---
+++ weRideApp/app/src/main/java/com/weride/werideapp/FindRideResultPage.java
@@ -0,0 +1,15 @@
+package com.weride.werideapp;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class FindRideResultPage extends AppCompatActivity {
+
+
+}
